c1423bb84cff9b5254d1b826d797544632faaeb5
TimekillerTK <erwartungen@protonmail.com>
2020-09-23 21:20:02 +0200

    added function to join two lists, not working, needs incorporating

--- Get-ADGroupUserInfo.ps1
+++ Get-ADGroupUserInfo.ps1
@@ -26,9 +26,71 @@ function Get-ADGroupUserInfo {
 
         $value = Get-ADGroupMember -Identity $Group -Recursive -Server $Server
         
-        $value | ForEach-Object -parallel {
-            # Add user here
+        # Spaghetti code below, which will need to be untangled and incorporated tomorrow:
+        $list1 = For ($i = 0; $i -lt 15; $i++ ) {
+            [pscustomobject]@{
+                Email = "$i@something.com"
+                HairColor = "blue","yellow","purple" | Get-Random
+                EyeColor = "yellow","blue","indigo" | Get-Random
+            }
         }
+        
+        $list2 = For ($i = 0; $i -lt 20; $i++ ) {
+            [pscustomobject]@{
+                Email = "$i@something.com"
+                Height = Get-Random -Minimum 100 -Maximum 220
+                Weight = Get-Random -Minimum 40 -Maximum 150
+            }
+        }
+        
+        $list1 = Import-Csv -Path .\csv\list1.csv
+        $list2 = Import-Csv -Path .\csv\list2.csv
+        
+        # Check each item in $list1 for Hair/Eye color
+        $combined = foreach ($item1 in $list1) {
+        
+            # Check each item in $list2 for Height/Weight
+            foreach ($item2 in $list2){
+        
+                if ($item2.Email -eq $item1.Email){
+                    [PSCustomObject]@{
+                        Email = $item1.Email
+                        HairColor = $item1.HairColor
+                        EyeColor = $item1.EyeColor
+                        Height = $item2.Height
+                        Weight = $item2.Weight
+                    }
+                } 
+        
+            }
+        
+        
+        }
+        $different1 = foreach ($item1 in $list1) {
+        
+            if (!($item1.Email -in $list2.Email)){
+                [PSCustomObject]@{
+                    Email = $item1.Email
+                    HairColor = $item1.HairColor
+                    EyeColor = $item1.EyeColor
+                }
+            }
+        
+        }
+        
+        $different2 = foreach ($item2 in $list2) {
+        
+            if (!($item2.Email -in $list1.Email)){
+                [PSCustomObject]@{
+                    Email = $item2.Email
+                    Height = $item2.Height
+                    Weight = $item2.Weight
+                }
+            }
+        
+        }
+        
+        $total = $combined + $different1 + $different2
 
     } #process
 } #function
