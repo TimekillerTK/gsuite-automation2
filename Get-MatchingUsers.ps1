ee2d95171700a31ec09589e54cf1d1873c32017a
TimekillerTK <erwartungen@protonmail.com>
2020-09-25 11:59:42 +0200

    renamed function, fully functional

--- Get-MatchingUsers.ps1
+++ Get-MatchingUsers.ps1
@@ -1,10 +1,9 @@
-# Get-ADGroupMember fetches AD group members, however with very limited information, 
-# this function will fetch specific properties of AD group members
+#Requires -Modules PSGsuite,ActiveDirectory
 
-function Get-ADGroupUserInfo {
+function Get-MatchingUsers {
     <#
     .SYNOPSIS
-    For fetching specific attribute info of AD group members (users)
+    Fo
     
     .DESCRIPTION
     Get-ADGroupMember fetches AD group members, however with very limited information, this function will fetch specific properties of AD group members
@@ -18,16 +17,11 @@ function Get-ADGroupUserInfo {
     [CmdletBinding()]
     param (
         [string]$GroupDN,
-        [string[]]$Attributes,
+        #[string[]]$Attributes,
         [string]$Server
     )
 
     PROCESS {
-
-        # $value = Get-ADGroupMember -Identity $Group -Recursive -Server $Server
-       # This works great now, but the ADuser search needs to be more refined to take into account nested groups
-        #$adusers = get-aduser -LDAPFilter "(memberof=$GroupDN)" -Server $Server -Properties mail    
-
 
 <# The querty below is probably the best one, the matching rule OID of 1.2.840.113556.1.4.1941 is a special "extended" match
 operator that walks the chain of ancestry in objects all the way to the root, until it finds a match.
@@ -42,25 +36,34 @@ To check for enabled, disabled and expiring users, check the UserAccountControl 
 * 512 = Enabled Account (normal account)
 * 16 = locked out
 * 
-
+This should be later changed to making an AD account show up as Enabled/Disabled
 #>
+        # First we need to gather information from both AD and GSuite about current users
         $adusers = Get-ADObject -LDAPFilter "(&(memberOf:1.2.840.113556.1.4.1941:=$GroupDN)(ObjectClass=user))" -Server $Server -Properties mail,objectsid,department,accountexpires,givenname,UserAccountControl
-        #$adusers = get-aduser -LDAPFilter "(memberof=$GroupDN)" -Server $Server -Properties mail
         $gsusers = Get-GSUser -filter *
 
-
+        # Checking for ADUsers that are have already been added to GSuite
         $combined = foreach ($aditem in $adusers) {
 
-            # Check each item in $gsusers for GSFirstName/GSLastName
+           
             foreach ($gsitem in $gsusers){
 
-        
+                # Checks if first part of E-mail i_ivanov@ has a match
                 if (($gsitem.user -replace "@(.*)") -eq ($aditem.mail -replace "@(.*)")){
+
+                    # Bandaid to fix some random user inserts, should be fed via parameter later on
+                    $lastname = $aditem.Name -replace "$($aditem.GivenName) "
+                    $lastname = $lastname -replace "\[Partner\] "
+                    $lastname = $lastname -replace "\(Blitz\)"
+                    $lastname = $lastname -replace "[0-9]$"
+
+                    # Creating the Object
                     [PSCustomObject]@{
                         ADSID = $aditem.objectsid
                         ADmail = $aditem.mail
                         ADFirstName = $aditem.GivenName
-                        ADLastName = $aditem.name -replace "$($aditem.givenname) " # This causes an issue with partner account names, investigate how to do it better!
+                        ADLastName = $lastname
+                        ADEnabled = $aditem.UserAccountControl
                         GSID = $gsitem.Id
                         GSmail = $gsitem.user
                         GSFirstName = $gsitem.name.GivenName
@@ -72,24 +75,36 @@ To check for enabled, disabled and expiring users, check the UserAccountControl 
         
         }
 
-    
+        # Checks for AD users which have not been added to GSuite yet
         $different1 = foreach ($aditem in $adusers) {
         
             # If the mail property of the $aditem iterated on is NOT in $gsusers.mail
             if (!(($aditem.mail -replace "@(.*)") -in ($gsusers.user -replace "@(.*)"))){
+
+                # Bandaid to fix some random user inserts, should be fed via parameter
+                $lastname = $aditem.Name -replace "$($aditem.GivenName) "
+                $lastname = $lastname -replace "\[Partner\] "
+                $lastname = $lastname -replace "\(Blitz\)"
+                $lastname = $lastname -replace "[0-9]$"            
+                
+                # Creating the Object
                 [PSCustomObject]@{
                     ADSID = $aditem.objectsid
                     ADmail = $aditem.mail
                     ADFirstName = $aditem.GivenName
-                    ADLastName = $aditem.name -replace "$($aditem.givenname) "
+                    ADLastName = $lastname
+                    ADEnabled = $aditem.UserAccountControl
                 }
             }
         
         }
         
+        # Checks for GSuite users that do not have a corresponding AD User account
         $different2 = foreach ($gsitem in $gsusers) {
         
+            # If the mail property of the $gsitem iterated on is NOT in $adusers.mail
             if (!(($gsitem.user -replace "@(.*)") -in ($adusers.mail -replace "@(.*)"))){
+                # Creating the Object
                 [PSCustomObject]@{
                     GSID = $gsitem.Id
                     GSmail = $gsitem.user
@@ -100,9 +115,24 @@ To check for enabled, disabled and expiring users, check the UserAccountControl 
         
         }
 
-        # Temp, should actually return a 'default' view.
-        return $combined + $different1 + $different2
+        # Combining everything
+        $total = $combined + $different1 + $different2
 
+        # First we need to set the default properties we want in a standard array
+        $defaultProperties = 'ADMail', 'GSMail'
+
+        # Now we set up a propertyset
+        $defaultPropertiesSet = New-Object System.Management.Automation.PSPropertySet(`
+            'DefaultDisplayPropertySet' `
+            ,[string[]]$defaultProperties `
+            )
+
+        # Create a PS Member Info object from the propertyset
+        $members = [System.Management.Automation.PSMemberInfo[]]@($defaultPropertiesSet)
+
+        # Once this is done, we can add this to
+        $total | Add-Member MemberSet PSStandardMembers $members
+        $total
 
 
     } #process
